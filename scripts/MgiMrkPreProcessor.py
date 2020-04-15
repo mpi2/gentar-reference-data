4f77602e1ef4ec8a703bc764bf284082e04bb26e
rwilson-ebi <rwilson@ebi.ac.uk>
2020-04-15 15:12:17 +0100

    updated the mgi and hgnc scripts, adding an extra field to the hgnc processor for agr information now found in the files.

--- scripts/MgiMrkPreProcessor.py
+++ scripts/MgiMrkPreProcessor.py
@@ -1,27 +1,30 @@
-import csv, sys, os, re
+import csv
+import os
+import re
+import sys
+
 
 class MgiMrkPreProcessor:
 
     def __init__(self):
-    
+
         self.file = sys.argv[1]
         self.pathname = os.path.dirname(self.file)
         self.basename = os.path.basename(self.file)
-        self.filename = os.path.abspath(os.path.join(self.pathname,self.basename))
-        
+        self.filename = os.path.abspath(os.path.join(self.pathname, self.basename))
+
         self.pattern = re.compile("o")
 
-        self.outputfilename = os.path.abspath(os.path.join(self.pathname,'Mrk_synonyms.txt'))
+        self.outputfilename = os.path.abspath(os.path.join(self.pathname, 'Mrk_synonyms.txt'))
 
         self.data = []
-        self.headings = ['MGI Accession ID','Chr','cM Position','genome coordinate start','genome coordinate end','strand','Marker Symbol','Status','Marker Name','Marker Type','Feature Type','Marker Synonyms (pipe-separated)']
-        
-        
-    
-    
-    def testHeadings(self, row, headings):
-        
-        
+        self.headings = ['MGI Accession ID', 'Chr', 'cM Position', 'genome coordinate start', 'genome coordinate end',
+                         'strand', 'Marker Symbol', 'Status', 'Marker Name', 'Marker Type', 'Feature Type',
+                         'Marker Synonyms (pipe-separated)']
+
+    @staticmethod
+    def test_headings(row, headings):
+
         if row != headings:
             print('The headings of the spreadsheet have changed')
             print('Expected:')
@@ -34,31 +37,27 @@ class MgiMrkPreProcessor:
             print('')
             print('******************')
             sys.exit('Headers have changed')
-    
-    
-    def readMrkFile(self):
+
+    def read_mrk_file(self):
         with open(self.filename, newline='') as f:
             reader = csv.reader(f, delimiter='\t')
             try:
-                counter=0
+                counter = 0
                 for row in reader:
-                    counter+=1
-                    
+                    counter += 1
+
                     # Ensure the expected columns are present
                     if counter == 1:
-                        self.testHeadings(row,self.headings)
-                    
-                    
+                        self.test_headings(row, self.headings)
+
                     # Load in the data rows, skipping the summary at the bottom
                     elif row[2]:
                         self.data.append(row)
-                    
-                    
+
             except csv.Error as e:
                 sys.exit('file {}, line {}: {}'.format(self.filename, reader.line_num, e))
-    
-    
-    def writeMrkFile(self):
+
+    def write_mrk_file(self):
         with open(self.outputfilename, 'w') as f:
             writer = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             try:
@@ -67,15 +66,15 @@ class MgiMrkPreProcessor:
                     for s in synonyms:
                         synonym = s.strip()
                         if synonym:
-                            id = row[0].strip()
-                            if id:
-                                writer.writerow([id,synonym])
-                    
+                            row_id = row[0].strip()
+                            if row_id:
+                                writer.writerow([row_id, synonym])
+
             except csv.Error as e:
                 sys.exit('file {}, line {}: {}'.format(self.outputfilename, writer.line_num, e))
-                
+
 
 if __name__ == '__main__':
-    l = MgiMrkPreProcessor()
-    l.readMrkFile()
-    l.writeMrkFile()
+    processor = MgiMrkPreProcessor()
+    processor.read_mrk_file()
+    processor.write_mrk_file()
