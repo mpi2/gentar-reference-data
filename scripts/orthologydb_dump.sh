148f00608db6aeb324921e47e32b9d372083a0e7
rwilson-ebi <rwilson@ebi.ac.uk>
2020-07-29 09:14:07 +0100

    Modified the orthologydb_dump.sh script to dump data directly from the orthology database.

--- scripts/orthologydb_dump.sh
+++ scripts/orthologydb_dump.sh
@@ -2,6 +2,7 @@
 set -e
 
 ENDPOINT="https://www.gentar.org/orthology-dev/v1alpha1/pg_dump"
+EBI_PROXY="http://hx-wwwcache.ebi.ac.uk:3128"
 
 error_exit()
 {
@@ -37,6 +38,18 @@ set_table_data_payload()
             }'
 }
 
+set_table_dump_options()
+{
+    if [ "$#" -ne 1 ]; then
+        error_exit "Usage: set_table_dump_options table";
+    fi;
+
+    table="$1"
+    
+	DUMPOPTIONS='-O -x --encoding=UTF8 --data-only --schema public --disable-triggers --table='"$table"
+	
+}
+
 set_table_filename()
 {
     if [ "$#" -ne 1 ]; then
@@ -57,9 +70,26 @@ fetch_data()
     request_payload="$1"
     output_file="$2"
 
-    curl "$ENDPOINT" -X POST --data "$request_payload" \
+    curl "$ENDPOINT" -X -x "$EBI_PROXY" POST --data "$request_payload" \
           -H "Content-Type: application/json" \
           -H "X-Hasura-Role: admin" > "$output_file"
+}
+
+
+pg_dump_data()
+{
+    if [ "$#" -ne 2 ]; then
+        error_exit "Usage: pg_dump_data dump_options output_file";
+    fi;
+
+    dump_options="$1"
+    output_file="$2"
+
+    pg_dump $dump_options \
+    -h $DATABASE_HOST \
+    -p $DATABASE_PORT \
+    -U $ORTHOLOGY_POSTGRES_USER \
+    -d $ORTHOLOGY_POSTGRES_DB > $output_file
 }
 
 dump_the_schema()
@@ -76,8 +106,8 @@ dump_table_data()
     for table in ortholog hgnc_gene mouse_gene mouse_gene_synonym mouse_gene_synonym_relation mouse_mapping_filter human_gene human_mapping_filter human_gene_synonym human_gene_synonym_relation; do
 		printf '%s:\n' "$table"
 		set_table_filename "$table"
-		set_table_data_payload "$table"
-		fetch_data "$PAYLOAD" "$OUTPUT"
+		set_table_dump_options "$table"
+		pg_dump_data "$DUMPOPTIONS" "$OUTPUT"
 		printf '\n'
 	done;
 }
